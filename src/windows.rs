e181ce3b8470942dbccf348d463ac060d6bc6a09
Mara Bos <m-ou.se@m-ou.se>
2022-09-27 16:16:06 +0200

    Fix Windows implementation.

--- src/windows.rs
+++ src/windows.rs
@@ -1,11 +1,11 @@
-use core::sync::atomic::{AtomicU32, Ordering::Relaxed};
+use core::sync::atomic::AtomicU32;
 use windows_sys::Win32::System::{
     Threading::{WaitOnAddress, WakeByAddressAll, WakeByAddressSingle},
     WindowsProgramming::INFINITE,
 };
 
 #[inline]
-pub fn wait(a: &AtomicU32, expected: u32) -> u32 {
+pub fn wait(a: &AtomicU32, expected: u32) {
     let ptr: *const AtomicU32 = a;
     let expected_ptr: *const u32 = &expected;
     unsafe { WaitOnAddress(ptr.cast(), expected_ptr.cast(), 4, INFINITE) };
